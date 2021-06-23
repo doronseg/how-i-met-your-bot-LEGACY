be092d476bfe687cd67730852fe08cb231fcc255
nerdoron <doron.segev1803@gmail.com>
2021-06-23 21:15:26 +0300

    test maven

---
+++ oscar/src/main/java/me/nerdoron/oscar/commands/useful/AboutCommand.java
@@ -0,0 +1,16 @@
+package me.nerdoron.oscar.commands.useful;
+
+import org.slf4j.Logger;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import me.nerdoron.oscar.commandManager.Command;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+public class AboutCommand extends Command {
+
+    @Override
+    public void execute(MessageReceivedEvent event, Object[] args, Logger logger, Dotenv dotenv) {
+
+    }
+
+}
