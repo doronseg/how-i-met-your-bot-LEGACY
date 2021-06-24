681c9ba3b9b96ebaabfbf7fff284766f78785465
nerdoron <doron.segev1803@gmail.com>
2021-06-24 11:39:59 +0300

    change json thing

--- oscar/src/main/java/me/nerdoron/oscar/commands/fun/Joke.java
+++ oscar/src/main/java/me/nerdoron/oscar/commands/fun/Joke.java
@@ -1,22 +1,13 @@
 package me.nerdoron.oscar.commands.fun;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.net.URL;
-import java.nio.charset.Charset;
-
-import com.fasterxml.jackson.databind.JsonNode;
-
+import org.json.JSONObject;
 import org.slf4j.Logger;
 
 import io.github.cdimascio.dotenv.Dotenv;
-import org.json.*;
 
 import me.nerdoron.oscar.Global;
 import me.nerdoron.oscar.commandManager.Command;
+import me.nerdoron.oscar.utils.JsonReader;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.TextChannel;
@@ -24,32 +15,11 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 public class Joke extends Command {
 
-    private static String readAll(Reader rd) throws IOException {
-        StringBuilder sb = new StringBuilder();
-        int cp;
-        while ((cp = rd.read()) != -1) {
-            sb.append((char) cp);
-        }
-        return sb.toString();
-    }
-
-    private static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
-        InputStream is = new URL(url).openStream();
-        try {
-            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
-            String jsonText = readAll(rd);
-            JSONObject json = new JSONObject(jsonText);
-            return json;
-        } finally {
-            is.close();
-        }
-    }
-
     @Override
     public void execute(MessageReceivedEvent event, Object[] args, Logger logger, Dotenv dotenv) {
         final TextChannel channel = event.getTextChannel();
         try {
-            JSONObject jo = readJsonFromUrl("https://official-joke-api.appspot.com/random_joke");
+            JSONObject jo = JsonReader.readJsonFromUrl("https://official-joke-api.appspot.com/random_joke");
             String setup = jo.getString("setup");
             String punch = jo.getString("punchline");
 
